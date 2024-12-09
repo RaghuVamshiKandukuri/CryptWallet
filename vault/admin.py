16f39f09278a2b3253fe2d49c45cc27e7b1b57db
shree ram <153072812+shreeram1001@users.noreply.github.com>
2024-12-09 23:43:25 +0530

    updated admin

--- vault/admin.py
+++ vault/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import AuditLog, UploadedFile, UserProfile
+
+admin.site.register(AuditLog)
+admin.site.register(UploadedFile)
+admin.site.register(UserProfile)
