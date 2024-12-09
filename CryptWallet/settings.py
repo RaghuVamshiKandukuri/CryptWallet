83cd8a7d9eb262262d10a462622bfac28aa3aca9
shree ram <153072812+shreeram1001@users.noreply.github.com>
2024-12-09 23:29:24 +0530

    allowed host *

--- CryptWallet/settings.py
+++ CryptWallet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0ru^-$sfxqp2dmjl6y(8v@desnlm%f8u%s(rb-j*qduz!6=jy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
